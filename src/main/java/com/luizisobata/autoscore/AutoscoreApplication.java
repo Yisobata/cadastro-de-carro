d46507599a0f81ef18c85d780946f14309d288ad
Luiz Felipe Isobata Yoshiassu <luiz.isobata@gmail.com>
2024-02-07 11:11:59 -0300

    Projeto criado

---
+++ src/main/java/com/luizisobata/autoscore/AutoscoreApplication.java
@@ -0,0 +1,13 @@
+package com.luizisobata.autoscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoscoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoscoreApplication.class, args);
+	}
+
+}
